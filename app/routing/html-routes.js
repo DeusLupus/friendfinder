a1072bff3a01808707c2c51f3223ed557e65790f
Deo Bissessar <Deo Bissessar>
2016-10-03 20:14:15 -0400

    added lines for survey page

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,9 +7,11 @@ module.exports = function(app){
 		var homepage = '/../public/home.html';
 		res.sendFile(path.join(__dirname + homepage));
 	});
+};
 
 module.exports = function(app){
+	app.get('/survey', function(req, res){
 		var surveypage = '/../public/survey.html';
 		res.sendFile(path.join(__dirname + surveypage));
-}
+	});
 };
